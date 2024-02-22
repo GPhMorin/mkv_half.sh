d3ec242386c9b5441de62c7ff4582f623157afa2
Gilles-Philippe Morin <6393838+GPhMorin@users.noreply.github.com>
2024-02-22 18:53:59 -0500

    Add files via upload

---
+++ mkv_half.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+file_pattern="$@"
+
+if [ ! $file_pattern ]; then
+  file_pattern="DSC*.avi DSC*.AVI"
+fi
+
+for file in $(ls -f $file_pattern 2> /dev/null)
+do
+  echo Checking $file
+  if [[ "$(file $file)" == *"video: Motion JPEG"* ]]; then
+    echo Processing $file
+    filenamebase=${file%.*}
+    echo Create right video: "$filenamebase"_r.MKV
+    ffmpeg -loglevel error -i $file -f matroska -qp 0 -map 0:v:1 -map 0:a:0 "$filenamebase"_r.MKV
+    echo Create left video: "$filenamebase"_l.MKV
+    ffmpeg -loglevel error -i $file -f matroska -qp 0 -map 0:v:0 -map 0:a:0 "$filenamebase"_l.MKV
+    echo Join left/right videos to a half SBS video: "$filenamebase"_HSBS.MKV
+    ffmpeg -loglevel error -i "$filenamebase"_l.MKV -i "$filenamebase"_r.MKV \
+	   -filter_complex "pad=in_w*2:in_h, overlay=main_w/2:0, scale=in_w/2:in_h" \
+	   -f matroska -pix_fmt yuv420p -b:v 28454K -aspect 32:9 -metadata:s:v stereo_mode=left_right -c:a copy -map 0:a:0 "$filenamebase"_HSBS.MKV
+  fi
+done
