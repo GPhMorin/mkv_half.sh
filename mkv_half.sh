dc6c8036bb448c006c98b9a8d48f1c8038e54fc1
Gilles-Philippe Morin <6393838+GPhMorin@users.noreply.github.com>
2024-02-22 21:15:08 -0500

    Update mkv_half.sh

--- mkv_half.sh
+++ mkv_half.sh
@@ -13,12 +13,12 @@ do
     echo Processing $file
     filenamebase=${file%.*}
     echo Create right video: "$filenamebase"_r.MKV
-    ffmpeg -loglevel error -i $file -f matroska -qp 0 -map 0:v:1 -map 0:a:0 "$filenamebase"_r.MKV
+    ffmpeg -loglevel error -i $file -f matroska -pix_fmt yuv420p -crf 2 -map 0:v:1 -map 0:a:0 -c:a copy "$filenamebase"_r.MKV
     echo Create left video: "$filenamebase"_l.MKV
-    ffmpeg -loglevel error -i $file -f matroska -qp 0 -map 0:v:0 -map 0:a:0 "$filenamebase"_l.MKV
+    ffmpeg -loglevel error -i $file -f matroska -pix_fmt yuv420p -crf 2 -map 0:v:0 -map 0:a:0 -c:a copy "$filenamebase"_l.MKV
     echo Join left/right videos to a half SBS video: "$filenamebase"_HSBS.MKV
     ffmpeg -loglevel error -i "$filenamebase"_l.MKV -i "$filenamebase"_r.MKV \
 	   -filter_complex "pad=in_w*2:in_h, overlay=main_w/2:0, scale=in_w/2:in_h" \
-	   -f matroska -pix_fmt yuv420p -b:v 28454K -aspect 32:9 -metadata:s:v stereo_mode=left_right -c:a copy -map 0:a:0 "$filenamebase"_HSBS.MKV
+	   -f matroska -crf 2 -aspect 32:9 -metadata:s:v stereo_mode=left_right -c:a copy -map 0:a:0 "$filenamebase"_HSBS.MKV
   fi
 done
